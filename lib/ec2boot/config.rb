f48e46dd7f9abbd68e186dc3b269d23cb54158a9
R.I.Pienaar <rip@devco.net>
2010-07-05 17:35:23 +0100

    rename comands dir to actions dir to keep naming consistant

--- lib/ec2boot/config.rb
+++ lib/ec2boot/config.rb
@@ -6,7 +6,7 @@ module EC2Boot
             @user_data_url = "http://169.254.169.254/latest/user-data"
             @meta_data_url = "http://169.254.169.254/latest/meta-data/"
             @cache_dir = "/var/spool/ec2boot"
-            @actions_dir = "/etc/ec2-boot-init/commands"
+            @actions_dir = "/etc/ec2-boot-init/actions"
             @facts_file = "/etc/facts.txt"
 
             @actions = Actions.new(self)
